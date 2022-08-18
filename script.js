833e63c30b514b3c779f60e4672219209900a9ae
Ruban Pavel <ruban.pavel@outlook.com>
2022-08-18 15:09:50 +0300

    base+JS

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let clearButtonName = document.getElementById("clearButtonName");
 let clearName = document.getElementById("Name");
+let clearButtonPassword = document.getElementById("clearButtonPass");
+let clearPassInput = document.getElementById("password");
 
 clearButtonName.addEventListener("click", clearButtonN);
 
 function clearButtonN() {
   clearName.value = "";
 }
-
-let clearButtonPassword = document.getElementById("clearButtonPass");
-let clearPassInput = document.getElementById("Password");
-
 clearButtonPassword.addEventListener("click", clearButtonP);
-
 function clearButtonP() {
   clearPassInput.value = "";
+}
+
+let changeShowButton = document.getElementById("showButton");
+let changePassword = document.getElementById("password");
+
+changeShowButton.addEventListener("click", changeButton);
+changePassword.addEventListener("click", changeButton);
+
+function changeButton() {
+  if (changeShowButton.innerHTML === "Show") {
+    changeShowButton.innerHTML = "Hide";
+    changePassword.type = "text";
+  } else {
+    changeShowButton.innerHTML = "Show";
+    changePassword.type = "password";
+  }
 }
